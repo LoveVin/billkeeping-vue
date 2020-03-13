4e90140971b52da1f78c8e4c7705cc5f5952aa43
twinklelixn <xiaonanli@163.com>
2020-03-13 16:18:48 +0800

    “创建rooter-view雏形

---
+++ src/views/Money.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Monet.vue
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+        name: 'Money'
+    };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
