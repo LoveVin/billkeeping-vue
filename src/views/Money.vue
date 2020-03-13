6db29b28de2cd61d4cb355118e452603744d44a4
twinklelixn <xiaonanli@163.com>
2020-03-13 17:01:48 +0800

    money页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,8 @@
 <template>
-    <div>
-        Monet.vue
+    <div class="nav-wrapper">
+        <div class="content">
+            Monet.vue
+        </div>
         <Nav/>
     </div>
 </template>
@@ -12,5 +14,14 @@
 </script>
 
 <style lang="scss" scoped>
-
+    .nav-wrapper{
+        display: flex;
+        border: 1px solid red;
+        flex-direction: column;
+        height: 100vh;
+    }
+    .content{
+        flex-grow: 1;
+        overflow: auto;
+    }
 </style>
