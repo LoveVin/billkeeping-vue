0405ff197348b2cbf9dfe579835d59e9be5a0e19
twinklelixn <xiaonanli@163.com>
2020-04-28 17:27:33 +0800

    改造 JS 为 TS

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -2,15 +2,41 @@
     <div>
         <ul class="types">
             <li :class="type === '-' && 'selected'"
-             @click="selectType('-')">支出</li>
+                @click="selectType('-')">支出
+            </li>
             <li :class="type === '+' && 'selected'"
-             @click="selectType('+')">收入</li>
+                @click="selectType('+')">收入
+            </li>
         </ul>
     </div>
 </template>
 
-<script>
-    export default {
+<script lang="ts">
+    import Vue from 'vue';
+    import {Component, Prop} from 'vue-property-decorator';
+
+    @Component //用于告诉ts下面的是一个Vue的组件,是一个装饰器，用于装饰下面的class
+    export default class Types extends Vue {
+        type = '-';
+
+        @Prop(Number) xxx: number | undefined;
+        //prop告诉 Vue : xxx 不是data 是 Prop
+        //Number 告诉 Vue xxx 是个 Number，运行时
+        //xxx 属性名
+        // number | undefined 告诉 TS xxx 的类型 编译时
+
+        selectType(type: string) {
+            if (type !== '-' && type !== '+') {
+                throw new Error('type is unknown');
+            }
+            this.type = type;
+        }
+        mounted(){
+            console.log(this.xxx);
+        }
+    }
+
+    /*export default {
         name: 'Types',
         data(){
             return {
@@ -25,23 +51,25 @@
                 this.type = type
             }
         }
-    };
+    };*/
 </script>
 
 <style lang="scss" scoped>
-    .types{
+    .types {
         background: #c4c4c4;
         display: flex;
         text-align: center;
         font-size: 24px;
-        > li{
+
+        > li {
             width: 50%;
             height: 64px;
             display: flex;
             justify-content: center;
             align-items: center;
             position: relative;
-            &.selected::after{
+
+            &.selected::after {
                 content: '';
                 position: absolute;
                 bottom: 0;
