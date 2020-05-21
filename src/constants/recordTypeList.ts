ffed8a36d7a7f75373f49db4fd77e380c6d06c9f
twinklelixn <xiaonanli@163.com>
2020-05-21 14:53:12 +0800

    在 Money.vue 里使用 Tabs

---
+++ src/constants/recordTypeList.ts
@@ -0,0 +1,6 @@
+//使用 Object.freeze的目的是为了防止数组被push，
+
+export default Object.freeze([
+    {text: '支出', value: '-'},
+    {text: '收入', value: '+'}
+]);
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,6 +1,6 @@
 <template>
     <Layout>
-        <Tabs class-prefix="type" :data-source="typeList" :value.sync="type"/>
+        <Tabs class-prefix="type" :data-source="recordTypeLIst" :value.sync="type"/>
         <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
         type:{{type}}
         <br/>
@@ -13,6 +13,8 @@
     import Vue from 'vue';
     import {Component} from 'vue-property-decorator';
     import Tabs from '@/components/Tabs.vue';
+    import intervalList from '@/constants/intervalList';
+    import recordTypeList from '@/constants/recordTypeList';
 
     @Component({
         components: {Tabs, Types}
@@ -20,15 +22,8 @@
     export default class Statistics extends Vue{
         type = '-';
         interval = 'day';
-        intervalList = [
-            {text: '按天',value: 'day'},
-            {text: '按周',value: 'week'},
-            {text: '按月',value: 'month'}
-        ];
-        typeList = [
-            {text: '支出', value: '-'},
-            {text: '收入', value: '+'}
-        ]
+        intervalList = intervalList;
+        recordTypeLIst = recordTypeList;
     }
 </script>
 
